2019bb6dddffbb388b652407f868ed7874c40f00
pavleen71 <149606091+pavleen71@users.noreply.github.com>
2024-04-08 00:34:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const requestBody = Object.fromEntries(formData);
 
         try {
-            const response = await fetch('http://localhost:7000/register', {
+            const response = await fetch('https://bvc-backend.onrender.com/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
